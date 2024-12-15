fe1c9fe4325a0315922b6cdf8dd51f4d58796e7b
Edvin Agnas <edvin.agnas@gmail.com>
2024-12-15 20:37:45 +0100

    Add async

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ esp_spp_role_t role = ESP_SPP_ROLE_SLAVE;  // or ESP_SPP_ROLE_MASTER
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("Starting...");
   if (!SerialBT.begin("ESP32-Scanner", true))
   {
     Serial.println("Failed to initialize Bluetooth");
@@ -25,34 +27,47 @@ void setup()
 
   Serial.println("Scanning for devices...");
   BTScanResults *btDeviceList = SerialBT.getScanResults(); // maybe accessing from different threads!
-  delay(BT_DISCOVER_TIME);
-  if (btDeviceList->getCount() > 0)
+  if (SerialBT.discoverAsync([](BTAdvertisedDevice *pDevice)
+                             {
+      // BTAdvertisedDeviceSet*set = reinterpret_cast<BTAdvertisedDeviceSet*>(pDevice);
+      // btDeviceList[pDevice->getAddress()] = * set;
+      Serial.printf(">>>>>>>>>>>Found a new device asynchronously: %s\n", pDevice->toString().c_str());
+      //Test to see that correct device is found, else reset and try again
+      String teststring= pDevice->toString().c_str();
   {
-    BTAddress addr;
-    int channel = 0;
-    Serial.println("Found devices:");
-    for (int i = 0; i < btDeviceList->getCount(); i++)
+    delay(BT_DISCOVER_TIME);
+    Serial.print("Stopping discoverAsync... ");
+    SerialBT.discoverAsyncStop();
+    Serial.println("discoverAsync stopped");
+    delay(1000);
+    if (btDeviceList->getCount() > 0)
     {
-      BTAdvertisedDevice *device = btDeviceList->getDevice(i);
-      Serial.printf(" ----- %s  %s %d\n", device->getAddress().toString().c_str(), device->getName().c_str(), device->getRSSI());
-      std::map<int, std::string> channels = SerialBT.getChannels(device->getAddress());
-      Serial.printf("scanned for services, found %d\n", channels.size());
-      for (auto const &entry : channels)
+      BTAddress addr;
+      int channel = 0;
+      Serial.println("Found devices:");
+      for (int i = 0; i < btDeviceList->getCount(); i++)
       {
-        Serial.printf("     channel %d (%s)\n", entry.first, entry.second.c_str());
-      }
-      if (channels.size() > 0)
-      {
-        addr = device->getAddress();
-        channel = channels.begin()->first;
+        BTAdvertisedDevice *device = btDeviceList->getDevice(i);
+        Serial.printf(" ----- %s  %s %d\n", device->getAddress().toString().c_str(), device->getName().c_str(), device->getRSSI());
+        std::map<int, std::string> channels = SerialBT.getChannels(device->getAddress());
+        Serial.printf("scanned for services, found %d\n", channels.size());
+        for (auto const &entry : channels)
+        {
+          Serial.printf("     channel %d (%s)\n", entry.first, entry.second.c_str());
+        }
+        if (channels.size() > 0)
+        {
+          addr = device->getAddress();
+          channel = channels.begin()->first;
+        }
       }
     }
-  }
-  else
-  {
-    Serial.println("Didn't find any devices, restarting...");
-    delay(500);
-    ESP.restart();
+    else
+    {
+      Serial.println("Didn't find any devices, restarting...");
+      delay(500);
+      ESP.restart();
+    }
   }
 }
 
